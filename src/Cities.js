91ca529483b333c19887ecd974206cdd2981b578
NastasiaKarpenko <nastasia.karpenko@gmail.com>
2022-12-01 10:31:41 +0000

    added Cities component

---
+++ src/Cities.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Cities.css";
+
+
+export default function Cities(){
+    return (
+            <ul className="Cities">
+        <li>Sydney</li>
+        <li>Paris</li>
+        <li>Montreal</li>
+        <li>Lisbon</li>
+        <li>San Francisco</li>
+      </ul>
+    );
+}
